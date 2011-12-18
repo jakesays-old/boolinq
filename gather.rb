6dbccb54fd02f7b76a0c1c60949ee692a7f4d49b
k06aaa <k06aaa@gmail.com>
2011-12-18 22:55:51 +0400

    Fixed some bugs in gather script

--- gather.rb
+++ gather.rb
@@ -6,9 +6,8 @@ HeaderFile = Struct.new(:name, :links, :content)
 include_std = [];
 file_list = [];
 
-for file in Dir.glob("*.h")
-    #puts "Processing file #{file} ..."
-    
+for file in Dir.glob("*.h").sort!{|a,b| a.length <=> b.length}
+    puts "Processing file #{file} ..."
     links = [];
     content = [];
     for line in File.open(file, "r")
@@ -29,24 +28,26 @@ end
 # Rearrange files
 
 index = 0
-for file in file_list
+while index < file_list.count
     max_index = index
-    for link in file.links
+    for link in file_list[index].links
         tmp_index = 0
         for f in file_list
             if f.name == link
                 break;
             end;
             tmp_index += 1
-        end
-        
+        end        
         max_index = [max_index, tmp_index].max
     end
     
     if index != max_index
         tmp = file_list[index]
-        file_list[index] = file_list[max_index]
-        file_list[max_index] = tmp
+        file_list.delete_at(index)
+        if (max_index > index)
+            max_index -= 1;
+        end
+        file_list.insert(max_index+1,tmp);
     end
     
     index += 1
@@ -58,10 +59,14 @@ Dir.chdir ".."
 ofile = File.open("boolinq-all.h", "w")
 ofile.puts "#pragma once"
 ofile.puts
+puts
 include_std.sort_by!{|a| a.size}
 for inc in include_std
+    puts "#{inc}"
     ofile.puts inc
 end
+puts
 for file in file_list
+    puts "#include \"#{file.name}\""
     ofile.puts file.content
 end
