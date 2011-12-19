285384c9aba33a582b9ae99b3e5c02541c9727ed
k06a <k06a@k06a>
2011-12-19 14:21:30 +0300

    Improved SelectRange tests

--- boolinq/SelectRangeTest.cpp
+++ boolinq/SelectRangeTest.cpp
@@ -3,6 +3,7 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "CommonTests.h"
 
 #include "IterRange.h"
 #include "SelectRange.h"
@@ -11,31 +12,31 @@ using namespace boolinq;
 
 TEST(SelectRange, Mul2)
 {
-    std::vector<int> src;
-    src.push_back(1);
-    src.push_back(2);
-    src.push_back(3);
-    src.push_back(4);
-    
+    int src[] = {1,2,3,4};
+    int ans[] = {2,4,6,8};
+
     auto rng = range(src);
     auto dst = select(rng, [](int a){return a * 2;});
 
-    EXPECT_EQ(2, dst.popFront());
-    EXPECT_EQ(4, dst.popFront());
-    EXPECT_EQ(6, dst.popFront());
-    EXPECT_EQ(8, dst.popFront());
-    EXPECT_TRUE(dst.empty());
+    CheckRangeAll(dst, ans);
 }
 
 TEST(SelectRange, MakeChar)
 {
-    std::vector<int> src;
-    src.push_back(1);
-    src.push_back(2);
-    src.push_back(3);
-    src.push_back(4);
+    int src[] = {1,2,3,4};
+    char ans[] = {'1','2','3','4'};
 
-    static const char * table[] =
+    auto rng = range(src);
+    auto dst = select(rng, [](int a){return (char)(a+'0');});
+
+    CheckRangeAll(dst, ans);
+}
+
+TEST(SelectRange, MakeString)
+{
+    int src[] = {1,2,3,4};
+
+    static std::string ans[] =
     {
         "hello",
         "world",
@@ -44,11 +45,7 @@ TEST(SelectRange, MakeChar)
     };
 
     auto rng = range(src);
-    auto dst = select(rng, [](int a){return table[a-1];});
+    auto dst = select(rng, [](int a){return ans[a-1];});
 
-    EXPECT_STREQ("hello", dst.popFront());
-    EXPECT_STREQ("world", dst.popFront());
-    EXPECT_STREQ("apple", dst.popFront());
-    EXPECT_STREQ("intel", dst.popFront());
-    EXPECT_TRUE(dst.empty());
+    CheckRangeAll(dst, ans);
 }
