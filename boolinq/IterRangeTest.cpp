8eb345df993824b1682c315cb6be5157ed144634
k06a <k06a@k06a>
2011-12-19 12:49:51 +0300

    Improved IterRangeTest's

--- boolinq/IterRangeTest.cpp
+++ boolinq/IterRangeTest.cpp
@@ -10,38 +10,26 @@
 
 using namespace boolinq;
 
-TEST(IterRange, IntVectorFront)
-{
-    std::vector<int> src;
-    src.push_back(1);
-    src.push_back(2);
-    src.push_back(3);
-    src.push_back(4);
+//////////////////////////////////////////////////////////////////////////
 
-    auto rng = range(src);
-    
-    for (int i = 1; i <= 4; i++)
+template<typename R>
+void IterRangeFront_rng12345(R rng)
+{
+    for (int i = 1; i <= 5; i++)
     {
         EXPECT_FALSE(rng.empty());
         EXPECT_EQ(i, rng.front());
-        EXPECT_EQ(4, rng.back());
+        EXPECT_EQ(5, rng.back());
         EXPECT_EQ(i, rng.popFront());
     }
 
     EXPECT_TRUE(rng.empty());
 }
 
-TEST(IterRange, IntVectorBack)
+template<typename R>
+void IterRangeBack_rng12345(R rng)
 {
-    std::vector<int> src;
-    src.push_back(1);
-    src.push_back(2);
-    src.push_back(3);
-    src.push_back(4);
-
-    auto rng = range(src);
-    
-    for (int i = 4; i >= 1; i--)
+    for (int i = 5; i >= 1; i--)
     {
         EXPECT_FALSE(rng.empty());
         EXPECT_EQ(1, rng.front());
@@ -54,24 +42,147 @@ TEST(IterRange, IntVectorBack)
 
 //////////////////////////////////////////////////////////////////////////
 
+TEST(IterRange, Rng12345)
+{
+    std::list<int> lst;
+        lst.push_back(1);
+        lst.push_back(2);
+        lst.push_back(3);
+        lst.push_back(4);
+        lst.push_back(5);
+    std::deque<int> dck;
+        dck.push_back(1);
+        dck.push_back(2);
+        dck.push_back(3);
+        dck.push_back(4);
+        dck.push_back(5);
+    std::vector<int> vec;
+        vec.push_back(1);
+        vec.push_back(2);
+        vec.push_back(3);
+        vec.push_back(4);
+        vec.push_back(5);
+    int arr[] = {1,2,3,4,5};
+    int * ptr = (int*)arr;
+
+
+    // Non-const front
+
+    IterRangeFront_rng12345(range(lst));
+    IterRangeFront_rng12345(range(lst.begin(), lst.end()));
+    IterRangeFront_rng12345(range(lst.cbegin(), lst.cend()));
+
+    IterRangeFront_rng12345(range(dck));
+    IterRangeFront_rng12345(range(dck.begin(), dck.end()));
+    IterRangeFront_rng12345(range(dck.cbegin(), dck.cend()));
+
+    IterRangeFront_rng12345(range(vec));
+    IterRangeFront_rng12345(range(vec.begin(), vec.end()));
+    IterRangeFront_rng12345(range(vec.cbegin(), vec.cend()));
+
+    IterRangeFront_rng12345(range(arr));
+    IterRangeFront_rng12345(range(ptr, 5));
+    IterRangeFront_rng12345(range(ptr, ptr+5));
+
+    // Non-const back
+
+    IterRangeBack_rng12345(range(lst));
+    IterRangeBack_rng12345(range(lst.begin(), lst.end()));
+    IterRangeBack_rng12345(range(lst.cbegin(), lst.cend()));
+
+    IterRangeBack_rng12345(range(dck));
+    IterRangeBack_rng12345(range(dck.begin(), dck.end()));
+    IterRangeBack_rng12345(range(dck.cbegin(), dck.cend()));
+
+    IterRangeBack_rng12345(range(vec));
+    IterRangeBack_rng12345(range(vec.begin(), vec.end()));
+    IterRangeBack_rng12345(range(vec.cbegin(), vec.cend()));
+
+    IterRangeBack_rng12345(range(arr));
+    IterRangeBack_rng12345(range(ptr, 5));
+    IterRangeBack_rng12345(range(ptr, ptr+5));
+}
+
+TEST(IterRange, ConstRng12345)
+{
+    std::list<const int> clst;
+        clst.push_back(1);
+        clst.push_back(2);
+        clst.push_back(3);
+        clst.push_back(4);
+        clst.push_back(5);
+    std::deque<const int> cdck;
+        cdck.push_back(1);
+        cdck.push_back(2);
+        cdck.push_back(3);
+        cdck.push_back(4);
+        cdck.push_back(5);
+    std::vector<const int> cvec;
+        cvec.push_back(1);
+        cvec.push_back(2);
+        cvec.push_back(3);
+        cvec.push_back(4);
+        cvec.push_back(5);
+    const int carr[] = {1,2,3,4,5};
+    const int * cptr = (const int*)carr;
+
+    // Const front
+
+    IterRangeFront_rng12345(range(clst));
+    IterRangeFront_rng12345(range(clst.begin(), clst.end()));
+    IterRangeFront_rng12345(range(clst.cbegin(), clst.cend()));
+
+    IterRangeFront_rng12345(range(cdck));
+    IterRangeFront_rng12345(range(cdck.begin(), cdck.end()));
+    IterRangeFront_rng12345(range(cdck.cbegin(), cdck.cend()));
+
+    IterRangeFront_rng12345(range(cvec));
+    IterRangeFront_rng12345(range(cvec.begin(), cvec.end()));
+    IterRangeFront_rng12345(range(cvec.cbegin(), cvec.cend()));
+
+    IterRangeFront_rng12345(range(carr));
+    IterRangeFront_rng12345(range(cptr, 5));
+    IterRangeFront_rng12345(range(cptr, cptr+5));
+    
+    // Const back
+
+    IterRangeBack_rng12345(range(clst));
+    IterRangeBack_rng12345(range(clst.begin(), clst.end()));
+    IterRangeBack_rng12345(range(clst.cbegin(), clst.cend()));
+
+    IterRangeBack_rng12345(range(cdck));
+    IterRangeBack_rng12345(range(cdck.begin(), cdck.end()));
+    IterRangeBack_rng12345(range(cdck.cbegin(), cdck.cend()));
+
+    IterRangeBack_rng12345(range(cvec));
+    IterRangeBack_rng12345(range(cvec.begin(), cvec.end()));
+    IterRangeBack_rng12345(range(cvec.cbegin(), cvec.cend()));
+
+    IterRangeBack_rng12345(range(carr));
+    IterRangeBack_rng12345(range(cptr, 5));
+    IterRangeBack_rng12345(range(cptr, cptr+5));
+    
+}
+
+//////////////////////////////////////////////////////////////////////////
+
 TEST(IterRange, OneElementFront)
 {
-    std::vector<int> src;
-    src.push_back(5);
-    
+    int src[] = {5};
+
     auto rng = range(src);
 
     EXPECT_FALSE(rng.empty());
     EXPECT_EQ(5, rng.front());
     EXPECT_EQ(5, rng.back());
     EXPECT_EQ(5, rng.popFront());
+
     EXPECT_TRUE(rng.empty());
 }
 
 TEST(IterRange, OneElementBack)
 {
-    std::vector<int> src;
-    src.push_back(5);
+    int src[] = {5};
 
     auto rng = range(src);
 
@@ -79,158 +190,13 @@ TEST(IterRange, OneElementBack)
     EXPECT_EQ(5, rng.front());
     EXPECT_EQ(5, rng.back());
     EXPECT_EQ(5, rng.popBack());
-    EXPECT_TRUE(rng.empty());
-}
-
-//////////////////////////////////////////////////////////////////////////
-
-TEST(IterRange, IntPointerFront)
-{
-    int src[] = {1,2,3,4,5};
-
-    auto rng = range((int*)src,(int*)src+5);
-
-    for(int i = 1; i <= 5; i++)
-    {
-        EXPECT_FALSE(rng.empty());
-        EXPECT_EQ(i, rng.front());
-        EXPECT_EQ(5, rng.back());
-        EXPECT_EQ(i, rng.popFront());
-    }
-
-    EXPECT_TRUE(rng.empty());
-}
-
-TEST(IterRange, IntPointerBack)
-{
-    int src[] = {1,2,3,4,5};
-
-    auto rng = range((int*)src,(int*)src+5);
-
-    for(int i = 5; i >= 1; i--)
-    {
-        EXPECT_FALSE(rng.empty());
-        EXPECT_EQ(1, rng.front());
-        EXPECT_EQ(i, rng.back());
-        EXPECT_EQ(i, rng.popBack());
-    }
 
     EXPECT_TRUE(rng.empty());
 }
 
 //////////////////////////////////////////////////////////////////////////
 
-TEST(IterRange, ConstIntPointerFront)
-{
-    int src[] = {1,2,3,4,5};
-    const int * src_ptr = (const int*)src;
-
-    auto rng = range(src_ptr,src_ptr+5);
-
-    for(int i = 1; i <= 5; i++)
-    {
-        EXPECT_FALSE(rng.empty());
-        EXPECT_EQ(i, rng.front());
-        EXPECT_EQ(5, rng.back());
-        EXPECT_EQ(i, rng.popFront());
-    }
-
-    EXPECT_TRUE(rng.empty());
-}
-
-TEST(IterRange, ConstIntPointerBack)
-{
-    int src[] = {1,2,3,4,5};
-    const int * src_ptr = (const int*)src;
-
-    auto rng = range(src_ptr,src_ptr+5);
-
-    for(int i = 5; i >= 1; i--)
-    {
-        EXPECT_FALSE(rng.empty());
-        EXPECT_EQ(1, rng.front());
-        EXPECT_EQ(i, rng.back());
-        EXPECT_EQ(i, rng.popBack());
-    }
-
-    EXPECT_TRUE(rng.empty());
-}
-
-//////////////////////////////////////////////////////////////////////////
-
-TEST(IterRange, IntArrayFront)
-{
-    int src[] = {1,2,3,4,5};
-
-    auto rng = range(src);
-
-    for(int i = 1; i <= 5; i++)
-    {
-        EXPECT_FALSE(rng.empty());
-        EXPECT_EQ(i, rng.front());
-        EXPECT_EQ(5, rng.back());
-        EXPECT_EQ(i, rng.popFront());
-    }
-
-    EXPECT_TRUE(rng.empty());
-}
-
-TEST(IterRange, IntArrayBack)
-{
-    int src[] = {1,2,3,4,5};
-
-    auto rng = range(src);
-
-    for(int i = 5; i >= 1; i--)
-    {
-        EXPECT_FALSE(rng.empty());
-        EXPECT_EQ(1, rng.front());
-        EXPECT_EQ(i, rng.back());
-        EXPECT_EQ(i, rng.popBack());
-    }
-
-    EXPECT_TRUE(rng.empty());
-}
-
-//////////////////////////////////////////////////////////////////////////
-
-TEST(IterRange, ConstIntArrayFront)
-{
-    const int src[] = {1,2,3,4,5};
-
-    auto rng = range(src);
-
-    for(int i = 1; i <= 5; i++)
-    {
-        EXPECT_FALSE(rng.empty());
-        EXPECT_EQ(i, rng.front());
-        EXPECT_EQ(5, rng.back());
-        EXPECT_EQ(i, rng.popFront());
-    }
-
-    EXPECT_TRUE(rng.empty());
-}
-
-TEST(IterRange, ConstIntArrayBack)
-{
-    const int src[] = {1,2,3,4,5};
-
-    auto rng = range(src);
-
-    for(int i = 5; i >= 1; i--)
-    {
-        EXPECT_FALSE(rng.empty());
-        EXPECT_EQ(1, rng.front());
-        EXPECT_EQ(i, rng.back());
-        EXPECT_EQ(i, rng.popBack());
-    }
-
-    EXPECT_TRUE(rng.empty());
-}
-
-//////////////////////////////////////////////////////////////////////////
-
-TEST(IterRange, NoElements)
+TEST(IterRange, EmptyVector)
 {
     std::vector<int> src;
     
