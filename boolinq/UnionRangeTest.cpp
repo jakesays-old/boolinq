621f6a44542eb8c33da9458f7041f0318600b109
k06a <k06a@k06a>
2011-12-19 14:25:04 +0300

    Improved UnionAllRange tests

--- boolinq/UnionRangeTest.cpp
+++ boolinq/UnionRangeTest.cpp
@@ -1,151 +1,60 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "CommonTests.h"
 
 #include "IterRange.h"
 #include "UnionAllRange.h"
 
 using namespace boolinq;
 
-TEST(UnionAllRange, TwoArraysFront)
+TEST(UnionAllRange, ArrayArray)
 {
-    int arr1[] = {1,2,3,4,5};
-    int arr2[] = {6,7,8,9};
+    int src1[] = {1,2,3,4,5};
+    int src2[] = {6,7,8,9};
 
-    auto rng1 = range(arr1);
-    auto rng2 = range(arr2);
+    int ans[] = {1,2,3,4,5,6,7,8,9};
+
+    auto rng1 = range(src1);
+    auto rng2 = range(src2);
     auto dst = unionAll(rng1,rng2);
-
-    for (int i = 1; i < 10; i++)
-    {
-        EXPECT_FALSE(dst.empty());
-        EXPECT_EQ(i, dst.front());
-        EXPECT_EQ(9, dst.back());
-        EXPECT_EQ(i, dst.popFront());
-    }
-
-    EXPECT_TRUE(dst.empty());
+             
+    CheckRangeAll(dst, ans);
 }
 
-TEST(UnionAllRange, TwoArraysBack)
+TEST(UnionAllRange, ArrayVector)
 {
-    int arr1[] = {1,2,3,4,5};
-    int arr2[] = {6,7,8,9};
+    int src1[] = {1,2,3,4,5};
+    std::vector<int> src2;
+    src2.push_back(6);
+    src2.push_back(7);
+    src2.push_back(8);
+    src2.push_back(9);
 
-    auto rng1 = range(arr1);
-    auto rng2 = range(arr2);
+    int ans[] = {1,2,3,4,5,6,7,8,9};
+
+    auto rng1 = range(src1);
+    auto rng2 = range(src2);
     auto dst = unionAll(rng1,rng2);
 
-    for (int i = 9; i > 0; i--)
-    {
-        EXPECT_FALSE(dst.empty());
-        EXPECT_EQ(1, dst.front());
-        EXPECT_EQ(i, dst.back());
-        EXPECT_EQ(i, dst.popBack());
-    }
-
-    EXPECT_TRUE(dst.empty());
+    CheckRangeAll(dst, ans);
 }
 
-//////////////////////////////////////////////////////////////////////////
-
-TEST(UnionAllRange, ArrayWithVectorFront)
+TEST(UnionAllRange, ArrayVectorArray)
 {
-    int arr1[] = {1,2,3,4,5};
-    std::vector<int> arr2;
-    arr2.push_back(6);
-    arr2.push_back(7);
-    arr2.push_back(8);
-    arr2.push_back(9);
+    int src1[] = {1,2,3,4,5};
+    std::vector<int> src2;
+    src2.push_back(6);
+    src2.push_back(7);
+    src2.push_back(8);
+    src2.push_back(9);
+    int src3[] = {10,11,12,13,14,15};
 
-    auto rng1 = range(arr1);
-    auto rng2 = range(arr2);
-    auto dst = unionAll(rng1,rng2);
+    int ans[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 
-    for (int i = 1; i < 10; i++)
-    {
-        EXPECT_FALSE(dst.empty());
-        EXPECT_EQ(i, dst.front());
-        EXPECT_EQ(9, dst.back());
-        EXPECT_EQ(i, dst.popFront());
-    }
-
-    EXPECT_TRUE(dst.empty());
-}
-
-TEST(UnionAllRange, ArrayWithVectorBack)
-{
-    int arr1[] = {1,2,3,4,5};
-    std::vector<int> arr2;
-    arr2.push_back(6);
-    arr2.push_back(7);
-    arr2.push_back(8);
-    arr2.push_back(9);
-
-    auto rng1 = range(arr1);
-    auto rng2 = range(arr2);
-    auto dst = unionAll(rng1,rng2);
-
-    for (int i = 9; i > 0; i--)
-    {
-        EXPECT_FALSE(dst.empty());
-        EXPECT_EQ(1, dst.front());
-        EXPECT_EQ(i, dst.back());
-        EXPECT_EQ(i, dst.popBack());
-    }
-
-    EXPECT_TRUE(dst.empty());
-}
-
-//////////////////////////////////////////////////////////////////////////
-
-TEST(UnionAllRange, ArrayVectorArrayFront)
-{
-    int arr1[] = {1,2,3,4,5};
-    std::vector<int> arr2;
-    arr2.push_back(6);
-    arr2.push_back(7);
-    arr2.push_back(8);
-    arr2.push_back(9);
-    int arr3[] = {10,11,12,13,14,15};
-
-    auto rng1 = range(arr1);
-    auto rng2 = range(arr2);
-    auto rng3 = range(arr3);
+    auto rng1 = range(src1);
+    auto rng2 = range(src2);
+    auto rng3 = range(src3);
     auto dst = unionAll(unionAll(rng1,rng2),rng3);
 
-    for (int i = 1; i < 16; i++)
-    {
-        EXPECT_FALSE(dst.empty());
-        EXPECT_EQ(i, dst.front());
-        EXPECT_EQ(15, dst.back());
-        EXPECT_EQ(i, dst.popFront());
-    }
-
-    EXPECT_TRUE(dst.empty());
-}
-
-TEST(UnionAllRange, ArrayVectorArrayBack)
-{
-    int arr1[] = {1,2,3,4,5};
-    std::vector<int> arr2;
-    arr2.push_back(6);
-    arr2.push_back(7);
-    arr2.push_back(8);
-    arr2.push_back(9);
-    int arr3[] = {10,11,12,13,14,15};
-
-    auto rng1 = range(arr1);
-    auto rng2 = range(arr2);
-    auto rng3 = range(arr3);
-    auto dst = unionAll(unionAll(rng1,rng2),rng3);
-
-    for (int i = 15; i > 0; i--)
-    {
-        EXPECT_FALSE(dst.empty());
-        EXPECT_EQ(1, dst.front());
-        EXPECT_EQ(i, dst.back());
-        EXPECT_EQ(i, dst.popBack());
-    }
-
-    EXPECT_TRUE(dst.empty());
+    CheckRangeAll(dst, ans);
 }
