264cff00a12228d91eb0eff4847b9e59e2f0a6b4
k06a <k06a@k06a>
2011-12-19 14:05:36 +0300

    Improved test for DistinctRange

--- boolinq/DistinctRangeTest.cpp
+++ boolinq/DistinctRangeTest.cpp
@@ -3,6 +3,7 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "CommonTests.h"
 
 #include "IterRange.h"
 #include "DistinctRange.h"
@@ -11,234 +12,60 @@ using namespace boolinq;
 
 //////////////////////////////////////////////////////////////////////////
 
-TEST(DistinctRange, IntsFront)
+TEST(DistinctRange, Ints1to6)
 {
-    std::vector<int> src;
-    src.push_back(4);
-    src.push_back(5);
-    src.push_back(3);
-    src.push_back(1);
-    src.push_back(4); // duplicate
-    src.push_back(2);
-    src.push_back(1); // duplicate
-    src.push_back(4); // duplicate
-    src.push_back(6);
+    int src[]   = {4,5,3,1,4,2,1,4,6};
+    int ansF[]  = {4,5,3,1,  2,    6};
+    int ansB[]  = {4,5,3,    2,1,  6};
+    int ansFB[] = {4,5,3,1,  2,    6};
+    int ansBF[] = {4,5,3,    2,1,  6};
 
     auto rng = range(src);
     auto dst = distinct(rng);
     
-    std::vector<int> ans;
-    ans.push_back(4);
-    ans.push_back(5);
-    ans.push_back(3);
-    ans.push_back(1);
-    //ans.push_back(4); // duplicate
-    ans.push_back(2);
-    //ans.push_back(1); // duplicate
-    //ans.push_back(4); // duplicate
-    ans.push_back(6);
-
-    for(unsigned i = 0; i < ans.size(); i++)
-    {
-        EXPECT_FALSE(dst.empty());
-        EXPECT_EQ(ans[i], dst.front());
-        EXPECT_EQ(6, dst.back());
-        EXPECT_EQ(ans[i], dst.popFront());
-    }
-    
-    EXPECT_TRUE(dst.empty());
+    CheckRangeFront(dst, ansF);
+    CheckRangeBack(dst, ansB);
+    CheckRangeFrontBack(dst, ansFB);
+    CheckRangeBackFront(dst, ansBF);
 }
-
-TEST(DistinctRange, IntsBack)
-{
-    std::vector<int> src;
-    src.push_back(4);
-    src.push_back(5);
-    src.push_back(3);
-    src.push_back(1);
-    src.push_back(4); // duplicate
-    src.push_back(2);
-    src.push_back(1); // duplicate
-    src.push_back(4); // duplicate
-    src.push_back(6);
-
-    auto rng = range(src);
-    auto dst = distinct(rng);
-    
-    std::vector<int> ans;
-    ans.push_back(4);
-    ans.push_back(5);
-    ans.push_back(3);
-    //ans.push_back(1); // duplicate
-    //ans.push_back(4); // duplicate
-    ans.push_back(2);
-    ans.push_back(1);
-    //ans.push_back(4); // duplicate
-    ans.push_back(6);
-
-    for(unsigned i = ans.size(); i > 0; i--)
-    {
-        EXPECT_FALSE(dst.empty());
-        EXPECT_EQ(4, dst.front());
-        EXPECT_EQ(ans[i-1], dst.back());
-        EXPECT_EQ(ans[i-1], dst.popBack());
-    }
-
-    EXPECT_TRUE(dst.empty());
-}
-
-//////////////////////////////////////////////////////////////////////////
 
 TEST(DistinctRange, IntMirrorFront)
 {
-    std::vector<int> src;
-    src.push_back(3);
-    src.push_back(2);
-    src.push_back(1);
-    src.push_back(0);
-    src.push_back(1);
-    src.push_back(2);
-    src.push_back(3);
-    
-    auto rng = range(src);
-    auto dst = distinct(rng);
-
-    std::vector<int> ans;
-    ans.push_back(2);
-    ans.push_back(1);
-    ans.push_back(0);
-    ans.push_back(3);
-    
-    for(unsigned i = 0; i < ans.size(); i++)
-    {
-        EXPECT_FALSE(dst.empty());
-        EXPECT_EQ(ans[i], dst.front());
-        EXPECT_EQ(3, dst.back());
-        EXPECT_EQ(ans[i], dst.popFront());
-    }
-
-    EXPECT_TRUE(dst.empty());
-}
-
-TEST(DistinctRange, IntMirrorBack)
-{
-    std::vector<int> src;
-    src.push_back(3);
-    src.push_back(2);
-    src.push_back(1);
-    src.push_back(0);
-    src.push_back(1);
-    src.push_back(2);
-    src.push_back(3);
+    int src[]   = {3,2,1,0,1,2,3};
+    int ansF[]  = {2,1,0,3};
+    int ansB[]  = {2,0,1,3};
+    int ansFB[] = {2,1,0,3};
+    int ansBF[] = {2,0,1,3};
 
     auto rng = range(src);
     auto dst = distinct(rng);
-    
-    std::vector<int> ans;
-    ans.push_back(2);
-    ans.push_back(0);
-    ans.push_back(1);
-    ans.push_back(3);
 
-    for(unsigned i = ans.size(); i > 0; i--)
-    {
-        EXPECT_FALSE(dst.empty());
-        EXPECT_EQ(2, dst.front());
-        EXPECT_EQ(ans[i-1], dst.back());
-        EXPECT_EQ(ans[i-1], dst.popBack());
-    }
-
-    EXPECT_TRUE(dst.empty());
+    CheckRangeFront(dst, ansF);
+    CheckRangeBack(dst, ansB);
+    CheckRangeFrontBack(dst, ansFB);
+    CheckRangeBackFront(dst, ansBF);
 }
-
-//////////////////////////////////////////////////////////////////////////
 
 TEST(DistinctRange, ManyEqualsFront)
 {
-    std::vector<int> src;
-    src.push_back(1);
-    src.push_back(1);
-    src.push_back(1);
-    src.push_back(1);
-    
-    auto rng = range(src);
-    auto dst = distinct(rng);
-    
-    EXPECT_FALSE(dst.empty());
-    EXPECT_EQ(1, dst.front());
-    EXPECT_EQ(1, dst.back());
-    EXPECT_EQ(1, dst.popFront());
-    
-    EXPECT_TRUE(dst.empty());
-}
-
-TEST(DistinctRange, ManyEqualsBack)
-{
-    std::vector<int> src;
-    src.push_back(1);
-    src.push_back(1);
-    src.push_back(1);
-    src.push_back(1);
+    int src[] = {1,1,1,1};
+    int ans[] = {1};
 
     auto rng = range(src);
     auto dst = distinct(rng);
-
-    EXPECT_FALSE(dst.empty());
-    EXPECT_EQ(1, dst.front());
-    EXPECT_EQ(1, dst.back());
-    EXPECT_EQ(1, dst.popBack());
-
-    EXPECT_TRUE(dst.empty());
+    
+    CheckRangeAll(dst, ans);
 }
-
-//////////////////////////////////////////////////////////////////////////
 
 TEST(DistinctRange, ManyEqualsWithOneFront)
 {
-    std::vector<int> src;
-    src.push_back(1);
-    src.push_back(1);
-    src.push_back(2);
-    src.push_back(1);
+    int src[] = {1,1,2,1};
+    int ans[] = {2,1};
 
     auto rng = range(src);
     auto dst = distinct(rng);
 
-    EXPECT_FALSE(dst.empty());
-    EXPECT_EQ(2, dst.front());
-    EXPECT_EQ(1, dst.back());
-    EXPECT_EQ(2, dst.popFront());
-
-    EXPECT_FALSE(dst.empty());
-    EXPECT_EQ(1, dst.front());
-    EXPECT_EQ(1, dst.back());
-    EXPECT_EQ(1, dst.popFront());
-
-    EXPECT_TRUE(dst.empty());
-}
-
-TEST(DistinctRange, ManyEqualsWithOneBack)
-{
-    std::vector<int> src;
-    src.push_back(1);
-    src.push_back(1);
-    src.push_back(2);
-    src.push_back(1);
-
-    auto rng = range(src);
-    auto dst = distinct(rng);
-
-    EXPECT_FALSE(dst.empty());
-    EXPECT_EQ(2, dst.front());
-    EXPECT_EQ(1, dst.back());
-    EXPECT_EQ(1, dst.popBack());
-
-    EXPECT_FALSE(dst.empty());
-    EXPECT_EQ(2, dst.front());
-    EXPECT_EQ(2, dst.back());
-    EXPECT_EQ(2, dst.popBack());
-
-    EXPECT_TRUE(dst.empty());
+    CheckRangeAll(dst, ans);
 }
 
 TEST(DistinctRange, OneFieldFront)
@@ -247,43 +74,29 @@ TEST(DistinctRange, OneFieldFront)
     {
         std::string name;
         int age;
-
-        Man(std::string name, int age)
-            : name(name), age(age)
-        {
-        }
     };
 
-    std::vector<Man> src;
-    src.push_back(Man("Anton",1));
-    src.push_back(Man("Taran",2));
-    src.push_back(Man("Poker",3));
-    src.push_back(Man("Agata",4));
-    src.push_back(Man("Mango",2));
-    src.push_back(Man("Banan",1));
+    Man src[] =
+    {
+        {"Anton",1},
+        {"Taran",2},
+        {"Poker",3},
+        {"Agata",4},
+        {"Mango",2},
+        {"Banan",1},
+    };
+
+    Man ans[] =
+    {
+        {"Taran",2},
+        {"Poker",3},
+        {"Agata",4},
+        {"Banan",1},
+    };
 
     auto rng = range(src);
     auto dst = distinct(rng, [](const Man & man){return man.age;});
 
-    EXPECT_FALSE(dst.empty());
-    EXPECT_EQ("Taran", dst.front().name);
-    EXPECT_EQ("Banan", dst.back().name);
-    EXPECT_EQ("Taran", dst.popFront().name);
-
-    EXPECT_FALSE(dst.empty());
-    EXPECT_EQ("Poker", dst.front().name);
-    EXPECT_EQ("Banan", dst.back().name);
-    EXPECT_EQ("Poker", dst.popFront().name);
-
-    EXPECT_FALSE(dst.empty());
-    EXPECT_EQ("Agata", dst.front().name);
-    EXPECT_EQ("Banan", dst.back().name);
-    EXPECT_EQ("Agata", dst.popFront().name);
-
-    EXPECT_FALSE(dst.empty());
-    EXPECT_EQ("Banan", dst.front().name);
-    EXPECT_EQ("Banan", dst.back().name);
-    EXPECT_EQ("Banan", dst.popFront().name);
-
-    EXPECT_TRUE(dst.empty());
+    CheckRangeFront(dst, ans, [](const Man & man){return man.name;});
+    CheckRangeBack(dst, ans, [](const Man & man){return man.name;});
 }
