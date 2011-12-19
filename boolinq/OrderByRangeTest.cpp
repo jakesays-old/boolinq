827651af044b9a1c9d9efdca3c498111cdce9db9
k06a <k06a@k06a>
2011-12-19 13:21:19 +0300

    Improved OrderByRange tests

--- boolinq/OrderByRangeTest.cpp
+++ boolinq/OrderByRangeTest.cpp
@@ -7,106 +7,93 @@
 #include "IterRange.h"
 #include "OrderByRange.h"
 
+#include "ReverseRange.h"
+#include "ToVector.h"
+
 using namespace boolinq;
 
-TEST(OrderByRange, RandomIntsWithDuplicatesFront)
+//////////////////////////////////////////////////////////////////////////
+
+template<typename R, typename T, int N, typename F>
+void CheckRangeFront(R dst, T (&ans)[N], F f)
 {
-    std::vector<int> src;
-    src.push_back(4);
-    src.push_back(5);
-    src.push_back(3);
-    src.push_back(1);
-    src.push_back(4); // duplicate
-    src.push_back(2);
-    src.push_back(1); // duplicate
-    src.push_back(4); // duplicate
-    src.push_back(6);
+    for (int i = 0; i < N; i++)
+    {
+        EXPECT_FALSE(dst.empty());
+        EXPECT_EQ(f(ans[i]),   f(dst.front()));
+        EXPECT_EQ(f(ans[N-1]), f(dst.back()));
+        EXPECT_EQ(f(ans[i]),   f(dst.popFront()));
+    }
 
-    auto rng = range(src);
-    auto dst = orderBy(rng);
-
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(1, dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(1, dst.popFront()); // duplicate
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(2, dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(3, dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(4, dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(4, dst.popFront()); // duplicate
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(4, dst.popFront()); // duplicate
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(5, dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(6, dst.popFront());
     EXPECT_TRUE(dst.empty());
 }
 
-TEST(OrderByRange, RandomIntsWithDuplicatesBack)
+template<typename R, typename T, int N, typename F>
+void CheckRangeBack(R dst, T (&ans)[N], F f)
 {
-    std::vector<int> src;
-    src.push_back(4);
-    src.push_back(5);
-    src.push_back(3);
-    src.push_back(1);
-    src.push_back(4); // duplicate
-    src.push_back(2);
-    src.push_back(1); // duplicate
-    src.push_back(4); // duplicate
-    src.push_back(6);
+    for (int i = N; i > 0; i--)
+    {
+        EXPECT_FALSE(dst.empty());
+        EXPECT_EQ(f(ans[0]),   f(dst.front()));
+        EXPECT_EQ(f(ans[i-1]), f(dst.back()));
+        EXPECT_EQ(f(ans[i-1]), f(dst.popBack()));
+    }
+
+    EXPECT_TRUE(dst.empty());
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+template<typename R, typename T, int N>
+void CheckRangeFront(R dst, T (&ans)[N])
+{
+    typedef typename R::value_type value_type;
+    CheckRangeFront(dst, ans, [](const value_type & a){return a;});
+}
+
+template<typename R, typename T, int N>
+void CheckRangeBack(R dst, T (&ans)[N])
+{
+    typedef typename R::value_type value_type;
+    CheckRangeBack(dst, ans, [](const value_type & a){return a;});
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+TEST(OrderByRange, RandomIntsWithDuplicates)
+{
+    int src[] = {4,5,3,1,4,2,1,4,6};
+    int ans[] = {1,1,2,3,4,4,4,5,6};
 
     auto rng = range(src);
     auto dst = orderBy(rng);
 
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(6, dst.popBack());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(5, dst.popBack());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(4, dst.popBack());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(4, dst.popBack()); // duplicate
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(4, dst.popBack()); // duplicate
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(3, dst.popBack());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(2, dst.popBack());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(1, dst.popBack());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(1, dst.popBack()); // duplicate
-    EXPECT_TRUE(dst.empty());
+    CheckRangeFront(dst, ans);
+    CheckRangeBack(dst, ans);
 }
 
 TEST(OrderByRange, ReverseInts)
 {
-    std::vector<int> src;
-    src.push_back(4);
-    src.push_back(3);
-    src.push_back(2);
-    src.push_back(1);
+    int src[] = {4,3,2,1};
+    int ans[] = {1,2,3,4};
 
     auto rng = range(src);
     auto dst = orderBy(rng);
 
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(1, dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(2, dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(3, dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ(4, dst.popFront());
-    EXPECT_TRUE(dst.empty());
+    CheckRangeFront(dst, ans);
+    CheckRangeBack(dst, ans);
 }
 
-TEST(OrderByRange, OneElementFront)
+TEST(OrderByRange, OneElement)
 {
-    std::vector<int> src;
-    src.push_back(5);
+    int src[] = {5};
+    int ans[] = {5};
     
     auto rng = range(src);
     auto dst = orderBy(rng);
 
-    EXPECT_FALSE(dst.empty());
-    EXPECT_EQ(5, dst.popFront());
-    EXPECT_TRUE(dst.empty());
-}
-
-TEST(OrderByRange, OneElementBack)
-{
-    std::vector<int> src;
-    src.push_back(5);
-
-    auto rng = range(src);
-    auto dst = orderBy(rng);
-
-    EXPECT_FALSE(dst.empty());
-    EXPECT_EQ(5, dst.popBack());
-    EXPECT_TRUE(dst.empty());
+    CheckRangeFront(dst, ans);
+    CheckRangeBack(dst, ans);
 }
 
 TEST(OrderByRange, NoElements)
@@ -119,46 +106,62 @@ TEST(OrderByRange, NoElements)
     EXPECT_TRUE(dst.empty());
 }
 
+//////////////////////////////////////////////////////////////////////////
+
 TEST(OrderByRange, RandomStringByContent)
 {
-    std::vector<std::string> src;
-    src.push_back("microsoft");
-    src.push_back("intel");
-    src.push_back("nokia");
-    src.push_back("apple");
-    src.push_back("oracle");
-    src.push_back("sun");
+    std::string src[] =
+    {
+        "microsoft",
+        "intel",
+        "nokia",
+        "apple",
+        "oracle",
+        "sun",
+    };
+
+    std::string ans[] =
+    {
+        "apple",
+        "intel",
+        "microsoft",
+        "nokia",
+        "oracle",
+        "sun",
+    };
 
     auto rng = range(src);
     auto dst = orderBy(rng);
 
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ("apple",     dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ("intel",     dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ("microsoft", dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ("nokia",     dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ("oracle",    dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ("sun",       dst.popFront());
-    EXPECT_TRUE(dst.empty());
+    CheckRangeFront(dst, ans);
+    CheckRangeBack(dst, ans);
 }
 
 TEST(OrderByRange, RandomStringByLength)
 {
-    std::vector<std::string> src;
-    src.push_back("microsoft");
-    src.push_back("intel");
-    src.push_back("nokia");
-    src.push_back("apple");
-    src.push_back("oracle");
-    src.push_back("sun");
+    std::string src[] =
+    {
+        "microsoft",
+        "intel",
+        "nokia",
+        "apple",
+        "oracle",
+        "sun",
+    };
+
+    std::string ans[] =
+    {
+        "sun",
+        "intel",
+        "nokia",
+        "apple",
+        "oracle",
+        "microsoft",
+    };
 
     auto rng = range(src);
     auto dst = orderBy(rng, [](std::string a){return a.size();});
 
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ("sun",       dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ("intel",     dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ("nokia",     dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ("apple",     dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ("oracle",    dst.popFront());
-    EXPECT_FALSE(dst.empty());  EXPECT_EQ("microsoft", dst.popFront());
-    EXPECT_TRUE(dst.empty());
+    CheckRangeFront(dst, ans, [](const std::string & s){return s.size();});
+    CheckRangeBack(dst, ans, [](const std::string & s){return s.size();});
 }
