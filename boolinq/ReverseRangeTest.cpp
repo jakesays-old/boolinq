fb748ca68d9adfe82d4ce95e9b8b566e1c03a5bb
k06a <k06a@k06a>
2011-12-19 14:07:43 +0300

    Improved tests for ReverseRange

--- boolinq/ReverseRangeTest.cpp
+++ boolinq/ReverseRangeTest.cpp
@@ -5,154 +5,31 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "CommonTests.h"
 
 #include "IterRange.h"
 #include "ReverseRange.h"
 
 using namespace boolinq;
 
-TEST(ReverseRange, IntVectorFront)
+TEST(ReverseRange, IntVector)
 {
-    //
-    // [1] [2] [3] [4]
-    //  b           f
-    //  b       f
-    //  b   f
-    //  bf
-    //
-    
-    std::vector<int> src;
-    src.push_back(1);
-    src.push_back(2);
-    src.push_back(3);
-    src.push_back(4);
+    int src[] = {1,2,3,4};
+    int ans[] = {4,3,2,1};
 
     auto rng = range(src);
     auto dst = reverse(rng);
 
-    for (int i = 4; i >= 1; i--)
-    {
-        EXPECT_FALSE(dst.empty());
-        EXPECT_EQ(i, dst.front());
-        EXPECT_EQ(1, dst.back());
-        EXPECT_EQ(i, dst.popFront());
-    }
-
-    EXPECT_TRUE(dst.empty());
+    CheckRangeAll(dst, ans);
 }
 
-TEST(ReverseRange, IntVectorBack)
+TEST(ReverseRange, DoubleReverse)
 {
-    //
-    // [1] [2] [3] [4]
-    //  b           f
-    //      b       f
-    //          b   f
-    //             bf
-    //
-
-    std::vector<int> src;
-    src.push_back(1);
-    src.push_back(2);
-    src.push_back(3);
-    src.push_back(4);
-
-    auto rng = range(src);
-    auto dst = reverse(rng);
-
-    for (int i = 1; i <= 4; i++)
-    {
-        EXPECT_FALSE(dst.empty());
-        EXPECT_EQ(4, dst.front());
-        EXPECT_EQ(i, dst.back());
-        EXPECT_EQ(i, dst.popBack());
-    }
-
-    EXPECT_TRUE(dst.empty());
-}
-
-TEST(ReverseRange, IntVectorFrontBack)
-{
-    //
-    // [1] [2] [3] [4]
-    //  b           f
-    //  b       f
-    //      b   f
-    //      bf
-    //
-    
-    std::vector<int> src;
-    src.push_back(1);
-    src.push_back(2);
-    src.push_back(3);
-    src.push_back(4);
-
-    auto rng = range(src);
-    auto dst = reverse(rng);
-
-    EXPECT_FALSE(dst.empty());
-    EXPECT_EQ(4, dst.front());
-    EXPECT_EQ(1, dst.back());
-    EXPECT_EQ(4, dst.popFront());
-
-    EXPECT_FALSE(dst.empty());
-    EXPECT_EQ(3, dst.front());
-    EXPECT_EQ(1, dst.back());
-    EXPECT_EQ(1, dst.popBack());
-
-    EXPECT_FALSE(dst.empty());
-    EXPECT_EQ(3, dst.front());
-    EXPECT_EQ(2, dst.back());
-    EXPECT_EQ(3, dst.popFront());
-
-    EXPECT_FALSE(dst.empty());
-    EXPECT_EQ(2, dst.front());
-    EXPECT_EQ(2, dst.back());
-    EXPECT_EQ(2, dst.popBack());
-
-    EXPECT_TRUE(dst.empty());
-}
-
-//////////////////////////////////////////////////////////////////////////
-
-TEST(ReverseRange, DoubleReverseFront)
-{
-    std::vector<int> src;
-    src.push_back(1);
-    src.push_back(2);
-    src.push_back(3);
-    src.push_back(4);
+    int src[] = {1,2,3,4};
+    int ans[] = {1,2,3,4};
 
     auto rng = range(src);
     auto dst = reverse(reverse(rng));
 
-    for (int i = 1; i <= 4; i++)
-    {
-        EXPECT_FALSE(dst.empty());
-        EXPECT_EQ(i, dst.front());
-        EXPECT_EQ(i, dst.popFront());
-    }
-
-    EXPECT_TRUE(dst.empty());
-}
-
-TEST(ReverseRange, DoubleReverseBack)
-{
-    std::vector<int> src;
-    src.push_back(1);
-    src.push_back(2);
-    src.push_back(3);
-    src.push_back(4);
-
-    auto rng = range(src);
-    auto dst = reverse(reverse(rng));
-
-    for (int i = 4; i >= 1; i--)
-    {
-        EXPECT_FALSE(dst.empty());
-        EXPECT_EQ(i, dst.back());
-        EXPECT_EQ(i, dst.popBack());
-    }
-
-    EXPECT_TRUE(dst.empty());
+    CheckRangeAll(dst, ans);
 }
