b446a2dbff771bd79d5033735d5fb4e7148b60e3
k06a <k06a@k06a>
2011-12-19 13:50:02 +0300

    Added checking FrontBack and BackFront

--- boolinq/CommonTests.h
+++ boolinq/CommonTests.h
@@ -31,10 +31,47 @@ void CheckRangeBack(R dst, T (&ans)[N], F f)
 }
 
 template<typename R, typename T, int N, typename F>
+void CheckRangeTwisted(R dst, T (&ans)[N], F f, int bit)
+{
+    int b = 0;
+    int e = N-1;
+
+    for (int i = 0; i < N; i++)
+    {
+        EXPECT_FALSE(dst.empty());
+        EXPECT_EQ(f(ans[b]), f(dst.front()));
+        EXPECT_EQ(f(ans[e]), f(dst.back()));
+
+        if (i % 2 == bit)
+            EXPECT_EQ(f(ans[b++]), f(dst.popFront()));
+        else
+            EXPECT_EQ(f(ans[e--]), f(dst.popBack()));
+
+        bit = 1 - bit;  // reverse bit
+    }
+
+    EXPECT_TRUE(dst.empty());
+}
+
+template<typename R, typename T, int N, typename F>
+void CheckRangeFrontBack(R dst, T (&ans)[N], F f)
+{
+    CheckRangeTwisted(dst, ans, f, 0);
+}
+
+template<typename R, typename T, int N, typename F>
+void CheckRangeBackFront(R dst, T (&ans)[N], F f)
+{
+    CheckRangeTwisted(dst, ans, f, 1);
+}
+
+template<typename R, typename T, int N, typename F>
 void CheckRangeAll(R dst, T (&ans)[N], F f)
 {
     CheckRangeFront(dst, ans, f);
     CheckRangeBack(dst, ans, f);
+    CheckRangeFrontBack(dst, ans, f);
+    CheckRangeBackFront(dst, ans, f);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -54,10 +91,24 @@ void CheckRangeBack(R dst, T (&ans)[N])
 }
 
 template<typename R, typename T, int N>
+void CheckRangeFrontBack(R dst, T (&ans)[N])
+{
+    typedef typename R::value_type value_type;
+    CheckRangeFrontBack(dst, ans, [](const value_type & a){return a;});
+}
+
+template<typename R, typename T, int N>
+void CheckRangeBackFront(R dst, T (&ans)[N])
+{
+    typedef typename R::value_type value_type;
+    CheckRangeBackFront(dst, ans, [](const value_type & a){return a;});
+}
+
+template<typename R, typename T, int N>
 void CheckRangeAll(R dst, T (&ans)[N])
 {
-    CheckRangeFront(dst, ans);
-    CheckRangeBack(dst, ans);
+    typedef typename R::value_type value_type;
+    CheckRangeAll(dst, ans, [](const value_type & a){return a;});
 }
 
 //////////////////////////////////////////////////////////////////////////
