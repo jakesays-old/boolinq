2184dae587440d0ebc82711235f0b3f8d3e2f0db
k06aaa <k06aaa@gmail.com>
2011-12-21 23:21:26 +0400

    Added some test for ToContainer

--- boolinq/ToContainerTest.cpp
+++ boolinq/ToContainerTest.cpp
@@ -4,11 +4,55 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "CommonTests.h"
 
 #include "IterRange.h"
 #include "ToContainer.h"
 
 using namespace boolinq;
+
+//////////////////////////////////////////////////////////////////////////
+
+TEST(ToContainer, Array2List)
+{
+    int src[] = {100,200,300};
+
+    auto rng = range(src);
+    auto dst = toContainer<std::list>(rng);
+
+    EXPECT_EQ(3, dst.size());
+    EXPECT_EQ(100, dst.front()); dst.pop_front();
+    EXPECT_EQ(200, dst.front()); dst.pop_front();
+    EXPECT_EQ(300, dst.front()); dst.pop_front();
+}
+
+TEST(ToContainer, Array2Deque)
+{
+    int src[] = {100,200,300};
+
+    auto rng = range(src);
+    auto dst = toContainer<std::deque>(rng);
+
+    EXPECT_EQ(3, dst.size());
+    EXPECT_EQ(100, dst.front()); dst.pop_front();
+    EXPECT_EQ(200, dst.front()); dst.pop_front();
+    EXPECT_EQ(300, dst.front()); dst.pop_front();
+}
+
+TEST(ToContainer, Array2Vector)
+{
+    int src[] = {100,200,300};
+
+    auto rng = range(src);
+    auto dst = toContainer<std::vector>(rng);
+
+    EXPECT_EQ(3, dst.size());
+    EXPECT_EQ(100, dst[0]);
+    EXPECT_EQ(200, dst[1]);
+    EXPECT_EQ(300, dst[2]);
+}
+
+//////////////////////////////////////////////////////////////////////////
 
 TEST(ToContainer, List2List)
 {
@@ -48,3 +92,5 @@ TEST(ToContainer, Vector2Vector)
 
     EXPECT_EQ(dst,src);
 }
+
+//////////////////////////////////////////////////////////////////////////
