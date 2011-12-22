8f078c84954a3a06f8719a803efc7a8049451263
k06aaa <k06aaa@gmail.com>
2011-12-22 22:34:05 +0400

    Improved BitsRange tests

--- boolinq/BitsRangeTest.cpp
+++ boolinq/BitsRangeTest.cpp
@@ -12,7 +12,18 @@ using namespace boolinq;
 
 //////////////////////////////////////////////////////////////////////////
 
-TEST(BitsRange, OneByteFLHL)
+TEST(BitsRange, OneByteDefault)
+{
+    unsigned char src[] = {0xAA};
+    int ans[] = {1,0,1,0,1,0,1,0};
+
+    auto rng = range(src);
+    auto dst = bits(rng);
+
+    CheckRangeEqArray(dst, ans);
+}
+
+TEST(BitsRange, OneByteHL)
 {
     unsigned char src[] = {0xAA};
     int ans[] = {1,0,1,0,1,0,1,0};
@@ -23,7 +34,7 @@ TEST(BitsRange, OneByteFLHL)
     CheckRangeEqArray(dst, ans);
 }
 
-TEST(BitsRange, OneByteFLLH)
+TEST(BitsRange, OneByteLH)
 {
     unsigned char src[] = {0xAA};
     int ans[] = {0,1,0,1,0,1,0,1};
@@ -36,7 +47,7 @@ TEST(BitsRange, OneByteFLLH)
 
 //////////////////////////////////////////////////////////////////////////
 
-TEST(BitsRange, OneIntFLHL)
+TEST(BitsRange, OneIntDefault)
 {
     unsigned int src[] = {0x12345678};
     int ans[] = 
@@ -48,12 +59,29 @@ TEST(BitsRange, OneIntFLHL)
     };
 
     auto rng = range(src);
-    auto dst = bits<HighToLow,FirstToLast>(rng);
+    auto dst = bits(rng);
 
     CheckRangeEqArray(dst, ans);
 }
 
-TEST(BitsRange, OneIntFLLH)
+TEST(BitsRange, OneIntHL)
+{
+    unsigned int src[] = {0x12345678};
+    int ans[] = 
+    {
+        0,1,1,1,1,0,0,0,
+        0,1,0,1,0,1,1,0,
+        0,0,1,1,0,1,0,0,
+        0,0,0,1,0,0,1,0,
+    };
+
+    auto rng = range(src);
+    auto dst = bits<HighToLow>(rng);
+
+    CheckRangeEqArray(dst, ans);
+}
+
+TEST(BitsRange, OneIntLH)
 {
     unsigned int src[] = {0x12345678};
     int ans[] = 
@@ -70,4 +98,70 @@ TEST(BitsRange, OneIntFLLH)
     CheckRangeEqArray(dst, ans);
 }
 
+//////////////////////////////////////////////////////////////////////////
 
+TEST(BitsRange, IntsDefault)
+{
+    unsigned int src[] = {0x12345678,0xAABBCCDD};
+    int ans[] = 
+    {
+        0,1,1,1,1,0,0,0, // 78
+        0,1,0,1,0,1,1,0, // 56
+        0,0,1,1,0,1,0,0, // 34
+        0,0,0,1,0,0,1,0, // 12
+
+        1,1,0,1,1,1,0,1, // DD
+        1,1,0,0,1,1,0,0, // CC
+        1,0,1,1,1,0,1,1, // BB
+        1,0,1,0,1,0,1,0, // AA
+    };
+
+    auto rng = range(src);
+    auto dst = bits(rng);
+
+    CheckRangeEqArray(dst, ans);
+}
+
+TEST(BitsRange, IntsHL)
+{
+    unsigned int src[] = {0x12345678,0xAABBCCDD};
+    int ans[] = 
+    {
+        0,1,1,1,1,0,0,0, // 78
+        0,1,0,1,0,1,1,0, // 56
+        0,0,1,1,0,1,0,0, // 34
+        0,0,0,1,0,0,1,0, // 12
+
+        1,1,0,1,1,1,0,1, // DD
+        1,1,0,0,1,1,0,0, // CC
+        1,0,1,1,1,0,1,1, // BB
+        1,0,1,0,1,0,1,0, // AA
+    };
+
+    auto rng = range(src);
+    auto dst = bits<HighToLow>(rng);
+
+    CheckRangeEqArray(dst, ans);
+}
+
+TEST(BitsRange, IntsLH)
+{
+    unsigned int src[] = {0x12345678,0xAABBCCDD};
+    int ans[] = 
+    {
+        0,0,0,1,1,1,1,0, // -87
+        0,1,1,0,1,0,1,0, // -65
+        0,0,1,0,1,1,0,0, // -43
+        0,1,0,0,1,0,0,0, // -21
+
+        1,0,1,1,1,0,1,1, // -DD
+        0,0,1,1,0,0,1,1, // -CC
+        1,1,0,1,1,1,0,1, // -BB
+        0,1,0,1,0,1,0,1, // -AA
+    };
+
+    auto rng = range(src);
+    auto dst = bits<LowToHigh>(rng);
+
+    CheckRangeEqArray(dst, ans);
+}
