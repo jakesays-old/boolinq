995f6743f329629fef96de1926e3c6c1f55b83f7
k06aaa <k06aaa@gmail.com>
2011-12-22 22:15:00 +0400

    Improved BytesRange tests

--- boolinq/BytesRangeTest.cpp
+++ boolinq/BytesRangeTest.cpp
@@ -12,6 +12,17 @@ using namespace boolinq;
 
 //////////////////////////////////////////////////////////////////////////
 
+TEST(BytesRange, OneByteDefault)
+{
+    unsigned char src[] = {0xAA};
+    int ans[] = {0xAA};
+
+    auto rng = range(src);
+    auto dst = bytes(rng);
+
+    CheckRangeEqArray(dst, ans);
+}
+
 TEST(BytesRange, OneByteFL)
 {
     unsigned char src[] = {0xAA};
@@ -35,6 +46,17 @@ TEST(BytesRange, OneByteLF)
 }
 
 //////////////////////////////////////////////////////////////////////////
+
+TEST(BytesRange, OneIntDefault)
+{
+    int src[] = {0x12345678};
+    int ans[] = {0x78,0x56,0x34,0x12};
+
+    auto rng = range(src);
+    auto dst = bytes(rng);
+
+    CheckRangeEqArray(dst, ans);
+}
 
 TEST(BytesRange, OneIntFL)
 {
@@ -60,6 +82,21 @@ TEST(BytesRange, OneIntLF)
 
 //////////////////////////////////////////////////////////////////////////
 
+TEST(BytesRange, IntsDefault)
+{
+    int src[] = {0x12345678, 0xAABBCCDD};
+    int ans[] = 
+    {
+        0x78,0x56,0x34,0x12,
+        0xDD,0xCC,0xBB,0xAA,
+    };
+
+    auto rng = range(src);
+    auto dst = bytes<FirstToLast>(rng);
+
+    CheckRangeEqArray(dst, ans);
+}
+
 TEST(BytesRange, IntsFL)
 {
     int src[] = {0x12345678, 0xAABBCCDD};
@@ -71,6 +108,21 @@ TEST(BytesRange, IntsFL)
 
     auto rng = range(src);
     auto dst = bytes<FirstToLast>(rng);
+
+    CheckRangeEqArray(dst, ans);
+}
+
+TEST(BytesRange, IntsLF)
+{
+    int src[] = {0x12345678, 0xAABBCCDD};
+    int ans[] = 
+    {
+        0x12,0x34,0x56,0x78,
+        0xAA,0xBB,0xCC,0xDD,
+    };
+
+    auto rng = range(src);
+    auto dst = bytes<LastToFirst>(rng);
 
     CheckRangeEqArray(dst, ans);
 }
