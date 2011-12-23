66c84e66a5b8c4625f135e92271dd05358563261
k06a <k06a@k06a>
2011-12-23 16:09:37 +0300

    Added .bytes() and .unbytes() calls tests

--- boolinq/DotCallTest.cpp
+++ boolinq/DotCallTest.cpp
@@ -13,6 +13,23 @@ using namespace boolinq;
 
 //////////////////////////////////////////////////////////////////////////
 
+TEST(DotCall, BytesRange)
+{
+    int src[] = {0xAABBCCDD};
+    int ansFL[] = {0xDD,0xCC,0xBB,0xAA};
+    int ansLF[] = {0xAA,0xBB,0xCC,0xDD};
+
+    auto dstFL1 = from(src).bytes();
+    auto dstFL2 = from(src).bytes<FirstToLast>();
+    auto dstLF1 = from(src).bytes<LastToFirst>();
+
+    CheckRangeEqArray(dstFL1, ansFL);
+    CheckRangeEqArray(dstFL2, ansFL);
+    CheckRangeEqArray(dstLF1, ansLF);
+}            
+
+//////////////////////////////////////////////////////////////////////////
+
 TEST(DotCall, BitsRangeHL)
 {
     int src[] = {0xAABBCCDD};
@@ -68,3 +85,24 @@ TEST(DotCall, BitsRangeLH)
     CheckRangeEqArray(dstFL2, ansFL);
     CheckRangeEqArray(dstLF1, ansLF);
 }
+
+//////////////////////////////////////////////////////////////////////////
+
+TEST(DotCall, UnbytesRange)
+{
+    unsigned char src[] = 
+    {
+        0x78,0x56,0x34,0x12,
+        0xDD,0xCC,0xBB,0xAA
+    };
+    int ansFL[] = {0x12345678,0xAABBCCDD};
+    int ansLF[] = {0x78563412,0xDDCCBBAA};
+
+    auto dstFL1 = from(src).unbytes<int>();
+    auto dstFL2 = from(src).unbytes<int,FirstToLast>();
+    auto dstLF1 = from(src).unbytes<int,LastToFirst>();
+
+    CheckRangeEqArray(dstFL1, ansFL);
+    CheckRangeEqArray(dstFL2, ansFL);
+    CheckRangeEqArray(dstLF1, ansLF);
+} 
